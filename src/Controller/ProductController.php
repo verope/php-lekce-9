c0c117b73cb0219619a27d4572e78ab8d3a6be70
Luděk Veselý <ludek.vesely@email.com>
2018-11-15 16:25:31 +0100

    add solution

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -24,6 +24,26 @@ class ProductController extends AbstractController
     }
 
     /**
+     * @Route("/{id}/edit", name="product_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Product $product): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
      * @Route("/new", name="product_new", methods="GET|POST")
      */
     public function new(Request $request): Response
